374cf32ecfa52d1f7bc5286e25b12184a7ddd8c2
Pascal Mengelt <pascal.mengelt@gmail.com>
2025-12-09 08:36:10 +0100

    Updated project.

--- helperCompany.scala
+++ helperCompany.scala
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S scala shebang
 // DO NOT ADJUST. This file is replaced by `./helperCompany.scala init`.
 
-//> using dep io.github.pme123::orchescala-helper:0.2.13
+//> using dep io.github.pme123::orchescala-helper:0.2.31
 
 import orchescala.helper.dev.DevCompanyHelper
 
