9110ea86e4c7284b04b17a96f649b59b46d981eb
Pascal Mengelt <pascal.mengelt@gmail.com>
2026-03-06 10:40:23 +0100

    feat: add OrderCreditcardW4SSimulation for W4S engine testing
    Agent-Id: agent-19e48e4b-11d7-4379-beb7-e76bc2900e3d
    Linked-Note-Id: 8f43471b-2521-45e5-a4bf-fc45292e7879

---
+++ projects/democompany-cards/03-simulation/src/test/scala/democompany/cards/simulation/OrderCreditcardW4SSimulation.scala
@@ -0,0 +1,8 @@
+package democompany.cards
+package simulation
+
+// ./helper.scala deploy OrderCreditcardW4SSimulation
+// simulation/test
+// simulation/testOnly *OrderCreditcardW4SSimulation
+class OrderCreditcardW4SSimulation extends OrderCreditcardSimulation, CompanyW4SSimulation
+
